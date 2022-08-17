4be21a60139d22b49926527f07150483ea84670a
BoyVOL <vladislav_limonov@mail.ru>
2022-08-17 17:47:35 +0900

    1

--- addons/CustomMovPlugin/CustomMovPlugin.cs
+++ addons/CustomMovPlugin/CustomMovPlugin.cs
@@ -10,14 +10,11 @@ public class CustomMovPlugin : EditorPlugin
         GD.Print("Plugin ready");
         AddCustomType("SwarmController","Node2D",GD.Load<Script>("res://addons/CustomMovPlugin/SwarmController/SwarmControllerNode.cs"),
         GD.Load<Texture>("res://addons/CustomMovPlugin/SwarmController/icon.png"));
-        AddCustomType("SwarmKreep","Node2D",GD.Load<Script>("res://addons/CustomMovPlugin/SwarmKreep/SwarmKreepNode.cs"),
-        GD.Load<Texture>("res://addons/CustomMovPlugin/SwarmKreep/icon.png"));
     }
 
     public override void _ExitTree()
     {
         base._ExitTree();
         RemoveCustomType("SwarmController");
-        RemoveCustomType("SwarmKreep");
     }
 }
