9dd44e9d20690bed6fd39b28d637e10ed2881220
BoyVOL <vladislav_limonov@mail.ru>
2023-01-16 11:31:15 +0900

    1

--- addons/CustomMovPlugin/CustomMovPlugin.cs
+++ addons/CustomMovPlugin/CustomMovPlugin.cs
@@ -4,16 +4,23 @@ using System;
 [Tool]
 public class CustomMovPlugin : EditorPlugin
 {    
-    string LibDir = "res://addons/CustomMovPlugin/";
+    public string LibDir = "res://addons/CustomMovPlugin/";
+
+    public void AddCustomType(string Type, string parent){
+        AddCustomType(Type,parent,GD.Load<Script>(LibDir+Type+"/"+Type+".cs"),
+        GD.Load<Texture>(LibDir+"/"+Type+"/icon.png"));
+    }
+
+    public void AddAutoloadSingleton(string Name){
+        AddAutoloadSingleton(Name,LibDir+"/AutoloadScenes/"+Name+".tscn");
+    }
+    
     public override void _EnterTree(){
         base._EnterTree();
         GD.Print("Plugin ready");
-        AddCustomType("SwarmController","Node2D",GD.Load<Script>("res://addons/CustomMovPlugin/SwarmController/SwarmControllerNode.cs"),
-        GD.Load<Texture>("res://addons/CustomMovPlugin/SwarmController/icon.png"));
-        AddCustomType("MoveAngleFollower","Node2D",GD.Load<Script>("res://addons/CustomMovPlugin/MoveAngleFollower/MoveAngleFollowerNode.cs"),
-        GD.Load<Texture>("res://addons/CustomMovPlugin/MoveAngleFollower/icon.png"));
-        AddCustomType("SecondOrder","Node2D",GD.Load<Script>("res://addons/CustomMovPlugin/SecondOrder/SecondOrderNode.cs"),
-        GD.Load<Texture>("res://addons/CustomMovPlugin/SecondOrder/icon.png"));
+        AddCustomType("SwarmController","Node2D");
+        AddCustomType("MoveAngleFollower","Node2D");
+        AddCustomType("SecondOrder","Node2D");
     }
 
     public override void _ExitTree()
